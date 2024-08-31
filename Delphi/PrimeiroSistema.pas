3a0a1044387b7544317993d7f4a897b9d4ab8ba4
Wagner Ferreira <127759066+Wagnerffadm@users.noreply.github.com>
2024-08-31 09:11:18 -0300

    Box-PrimeiroSistema

---
+++ Delphi/PrimeiroSistema.pas
@@ -0,0 +1,35 @@
+unit PrimeiroSistema;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TPrimeiroSistema = class(TForm)
+    Label1: TLabel;
+    CaixaNome: TEdit;
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+    procedure CaixaNomeChange(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  PrimeiroSistema: TPrimeiroSistema;
+
+implementation
+
+{$R *.dfm}
+
+procedure TPrimeiroSistema.Button1Click(Sender: TObject);
+
+  begin
+    ShoWMessage('Olá '+ CaixaNome.Text);
+
+  end;
+  end.
